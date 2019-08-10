49e74d5726f86fda06cc8eeccb72cc76d3d5884f
cheneym20 <49948420+cheneym20@users.noreply.github.com>
2019-08-10 11:35:54 -0600

    Updating code, but still need help with eventlisten not working

--- index.js
+++ index.js
@@ -41,34 +41,63 @@ You can remove this script and any reference to it.
  }
 
 function keepTrackList(addRemove, testNbr){
-    if(testNbr == -1) return;
-    arr = [];
+    if(addRemove == "addNumber"){
+        if(problemList.length == 0){
+            problemList.push(1);
+            return 1;
+        } 
+        else if(problemList.length > 0){
+            let newNumber = Math.max(...problemList) + 1;
+            problemList.push(newNumber);
+            return newNumber;
+        }
+        
+    } else
+    if(addRemove == "subtractNumber"){
+        problemList.splice[problemList.indexOf(testNbr)];
+        return;
+    }
 }
 
  //This function will activate the test on the main screen with the proper test level.
  function addTest(level,opr){
-     //let testNbr = keepTrackList("add", -1);
-     document.getElementById("testLevelMain").style.display="block";
-     //document.getElementById("testLevelMain"+testNbr).style.height="60px";
+    let testNbr = keepTrackList("addNumber");
+    addMainDiv(testNbr);
+    let newDiv = document.getElementById("testLevelMain"+testNbr);
+    newDiv.style.display="block";
+    newDiv.style.height="55px";
 
-    let answer = addProblem(level,opr);
-    addInput();
-    addDoneBtn(answer);
-    addErrorMessage();
+    let answer = addProblem(level,opr,testNbr);
+    addInput(testNbr);
+    addDoneBtn(answer,testNbr);
+    addErrorMessage(testNbr);
  }
 
  function subTest(level,opr){
-    document.getElementById("testLevelMain").style.display="block";
-    //document.getElementById("testLevelMain").style.height="100px";
+    let testNbr = keepTrackList("addNumber");
+    addMainDiv(testNbr);
+    let newDiv = document.getElementById("testLevelMain"+testNbr);
+    newDiv.style.display="block";
+    newDiv.style.height="55px";
 
-    let answer = addProblem(level,opr);
-    //document.getElementById("math_question").style.display="inline-block";
-    addInput();
-    addDoneBtn(answer);
-    addErrorMessage();
+    let answer = addProblem(level,opr,testNbr);
+    addInput(testNbr);
+    addDoneBtn(answer,testNbr);
+    addErrorMessage(testNbr);
  }
 
-function addProblem(level,opr){
+ function addMainDiv(testNbr){
+     let mainDivTag = document.getElementById("testLevelMain");
+     let childDivTag = document.createElement("div");
+     setTimeout(function(){
+        childDivTag.className += '  add_transition';
+    }, 500);
+     childDivTag.setAttribute("class","main_body");
+     childDivTag.setAttribute("id","testLevelMain"+testNbr)
+     mainDivTag.parentNode.insertBefore(childDivTag, mainDivTag.nextSibling);
+ }
+
+function addProblem(level,opr,testNbr){
     let operator = opr;
     if(opr > 0) operator = "add";
     if(opr < 0) operator = "sub";
@@ -78,37 +107,39 @@ function addProblem(level,opr){
     let answer = parseInt(arr[0]) + parseInt(arr[1]);
     let tag = document.createElement("span");
     tag.appendChild(problem);
-    tag.setAttribute("id","testQuestion");
+    tag.setAttribute("id","testQuestion"+testNbr);
     tag.setAttribute("class","math_question");
-    let mainDivProb = document.getElementById("testLevelMain");
+    tag.style.display="inline-block";
+    let mainDivProb = document.getElementById("testLevelMain"+testNbr);
     mainDivProb.appendChild(tag);
     
     return answer;
 }
 
-function addInput(){
+function addInput(testNbr){
     let ansInput = document.createElement("input");
-    ansInput.setAttribute("id","answerInput");
-    let mainDivAnsInput = document.getElementById("testLevelMain");
+    ansInput.setAttribute("id","answerInput"+testNbr);
+    let mainDivAnsInput = document.getElementById("testLevelMain"+testNbr);
     mainDivAnsInput.appendChild(ansInput);
 }
 
-function addDoneBtn(answer){
+function addDoneBtn(answer,testNbr){
     let btnDone = document.createElement("button");
     let btnTxt = document.createTextNode("Done");
     btnDone.appendChild(btnTxt);
+    btnDone.setAttribute("type","button");
     btnDone.setAttribute("class","done_button");
-    btnDone.setAttribute("id","answer");
-    let mainDivBtnDone = document.getElementById("testLevelMain");
+    btnDone.setAttribute("id","answer"+testNbr);
+    btnDone.addEventListener("click",submitAnswer);
+
+    let mainDivBtnDone = document.getElementById("testLevelMain"+testNbr);
     mainDivBtnDone.appendChild(btnDone);
-    let userInput = -1;
-    document.getElementById("answer").addEventListener("click",submitAnswer(answer, userInput));
 }
 
-function addErrorMessage(){
+function addErrorMessage(testNbr){
     let errorPara = document.createElement("p");
-    errorPara.setAttribute("id","errorMessage");
-    let mainDivErrorPara = document.getElementById("testLevelMain");
+    errorPara.setAttribute("id","errorMessage"+testNbr);
+    let mainDivErrorPara = document.getElementById("testLevelMain"+testNbr);
     mainDivErrorPara.appendChild(errorPara);
 }
 
@@ -122,15 +153,14 @@ function submitAnswer(answer, userInput){
     } else
     if(answer == userInput){
         hideBoxWithCorrect();
-        keepTrackList("add");
+        keepTrackList("addNumber");
     }
 
     //let userInput = document.getElementById("answerInput").nodeValue;
 }
  
 function hideBoxWithCorrect(){
-    let problemBoxHeight = document.getElementById("testLevelMain").style.height = "0px";
-
+    document.getElementById("testLevelMain").style.height = "0px";
 }
 
 //This function will load all the proper information on the page when page loads.
@@ -150,10 +180,8 @@ function hideBoxWithCorrect(){
 
 //This function returns both the Math question String and the answer.
 function randomMathQuestionGenerator(level, operator){
-    console.log("operator: "+operator);
     let number1 = Math.floor(Math.random() * (level * 10));
     let number2;
-    console.log("number2: "+number2);
     if(operator == "add"){
         number2 = Math.floor(Math.random() * (level * 10));
     }
@@ -175,8 +203,5 @@ function randomMathQuestionGenerator(level, operator){
 
 }
 
-function randomMathQuestionGeneratorAnswer(){
-
-}
-
+let problemList = [];
 onload();
